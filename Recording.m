6d50880c10c10cded8ba7840801010806ec4d7f4
Aman Aberra <aaberra0@gmail.com>
2021-09-17 12:04:15 -0400

    Don't flip y axis by default

--- Recording.m
+++ Recording.m
@@ -62,10 +62,10 @@ classdef Recording < matlab.mixin.Copyable % Stack of images from recording
             end
             if strcmp(obj.format,'fits')
                 obj.vals = fitsread(obj.filepath); 
-                if ispc
-                    obj.vals = flipud(obj.vals); 
-                    fprintf('Flipping y axis, check!!\n'); 
-                end
+%                 if ispc
+%                     obj.vals = flipud(obj.vals); 
+%                     fprintf('Flipping y axis, check!!\n'); 
+%                 end
             else
                error('Other file formats not implemented yet');  
             end
