1e82f96dee96e1afdbf295fa4abcb935f01fa806
Aman Aberra <aaberra0@gmail.com>
2021-09-28 19:10:14 -0400

    Add extractAPsFromTrain function to find APs using findpeaks and extract window around each AP, plots averaged waveform

--- calcROIfuncs.m
+++ calcROIfuncs.m
@@ -40,15 +40,17 @@ end
 switch roi_func_mode    
     case 'combine'
         output = struct; 
-        mask = rois.getMask(img.imsize(1:2),1:rois.num_rois); % single mask for all ROIs                         
-        for i = 1:length(funcs) % add function output to struct in corresponding field
+        mask = rois.getMask(img.imsize(1:2),1:rois.num_rois); % single mask for all ROIs           
+        num_masks = 1;
+        for i = 1:length(funcs) % add function output to struct in corresponding field            
             output = apply_func(output,1,funcs{i},img.vals,mask,baseline_wind_inds); 
         end
     case 'separate' % separate mask for each roi
-        output = struct;                         
-        for j = 1:rois.num_rois
-            for i = 1:length(funcs)      
-                maskj = rois.getMask(img.imsize(1:2),j);                
+        output = struct;     
+        num_masks = rois.num_rois;
+        for j = 1:num_masks
+            maskj = rois.getMask(img.imsize(1:2),j);                
+            for i = 1:length(funcs)                                   
                 output = apply_func(output,j,funcs{i},img.vals,maskj,baseline_wind_inds); 
             end
         end
@@ -62,22 +64,36 @@ output.funcs = funcs;
 output.baseline_wind_inds = baseline_wind_inds; 
 function output_new = apply_func(output,ind,func,img,mask,bsline_wind)    
     output_new = output; 
-    if strcmp(func,'mean') % spatial mean across all rois                             
+    if strcmp(func,'mean') % spatial mean across all rois     
+        if ind == 1
+            output_new.mean = zeros(size(img,3),num_masks); % initialize
+        end
         output_new.mean(:,ind) = squeeze(mean(img.*mask,[1 2],'omitnan'));         
     elseif strcmp(func,'std')% spatial std across all rois        
+        if ind == 1
+            output_new.std = zeros(size(img,3),num_masks); % initialize
+        end
         output_new.std(:,ind) = squeeze(std(img.*mask,0,[1 2],'omitnan')); 
     elseif strcmp(func,'baseline') % Baseline value within ROI pixels across baseline time window
-        output_new.baseline(:,ind) = mean(img(:,:,bsline_wind(1,:) ).*mask,'all','omitnan');        
+        if ind == 1
+            output_new.baseline = zeros(size(bsline_wind,1),num_masks); % rows are for each stimulus, columns for rois        
+        end
+        for k = 1:size(bsline_wind,1)
+            output_new.baseline(k,ind) = mean(img(:,:,bsline_wind(k,:) ).*mask,'all','omitnan');        
+        end
     elseif strcmp(func,'deltaF_F0') % DeltaF/F0 of ROI pixels (averaged)
         if isfield(output,'mean')
             output_mean = output.mean(:,ind);
         else
             output_mean = squeeze(mean(img.*mask,[1 2],'omitnan'));  
         end
-        if isfield(output,'baseline')
-            baseline = output.baseline(:,ind);
+        if isfield(output,'baseline') % use baseline of first stim for global deltaF/F0 peak
+            baseline = output.baseline(1,ind);
         else
-            baseline = mean(img(:,:,bsline_wind(1,:)).*mask,'all','omitnan');        
+            baseline = mean(img(:,:,bsline_wind(1,:)).*mask,'all','omitnan');    
+        end
+        if ind == 1
+            output_new.deltaF_F0 = zeros(size(img,3),num_masks); % rows are for each stimulus, columns for rois        
         end
         output_new.deltaF_F0(:,ind) = (output_mean - baseline)./baseline;
     end
---
+++ extractAPsFromTrain.m
@@ -0,0 +1,107 @@
+function extractAPsFromTrain(means,baselines,exp_settings,varargin)
+%EXTRACTAPSFROMTRAIN ... 
+%  
+%   Inputs 
+%   ------ 
+%   Optional Inputs 
+%   --------------- 
+%   Outputs 
+%   ------- 
+%   Examples 
+%   --------------- 
+
+% AUTHOR    : Aman Aberra 
+in.AP_window = 50e-3; % time window (or frames) around AP peak to extract, specify units below
+                      % E.g. 50e-3 gives 25 ms before and after peak of
+                      % each AP
+in.AP_window_units = 'sec'; % specify units of AP_window, 'sec' or 'frames'
+in.min_interAP_interval = 0.95; % min time between APs defined as proportion of inter-stimulus interval
+in.min_AP_width = 0.3e-3; % min AP width (sec)
+in.plot_roi_ind = 1; % specify ROIs to plot in 2nd subplot (averaged waveforms overlaid)
+in.font_size = 16; 
+in.print_level = 1;
+in = sl.in.processVarargin(in,varargin); 
+% Get frames vector and stimulus vector (frames when stimuli occurred)
+exp_settings.convert2Frames(); % make sure units are in frames
+stim_frames = exp_settings.stim_vals;
+frames = (1:length(means));
+% t = (0:(length(means)-1))/exp_settings.sampling_rate;
+% Get frames and data post first stimulus
+frames_post_stim = frames(frames>stim_frames(1)); 
+means_post_stim = means(frames>stim_frames(1),:);
+% stim_times = stim_frames/exp_settings.sampling_rate; % WARNING: MAY BE INACCURATE
+% stim_times = (stim_delay:(1/stim_freq):t_frames(end));
+% Get size of window around each AP to extract in frames
+if strcmp(in.AP_window_units,'sec') % convert to frames
+    numAPframes = ceil(in.AP_window*exp_settings.sampling_rate);
+elseif strcmp(in.AP_window_units,'frames')
+    numAPframes = in.AP_window;
+end
+if mod(numAPframes,2) == 0
+   numAPframes = numAPframes + 1;  % make odd
+end
+ISI = min(diff(stim_frames));
+%% Find peaks
+num_rois = size(means_post_stim,2);
+peak_vals_all = cell(1,num_rois);
+peak_frames_all = cell(1,num_rois);
+for i = 1:num_rois
+    [peak_vals,peak_frames] = findpeaks(means_post_stim(:,i),frames_post_stim,...
+                                  'NPeaks',length(stim_frames),...
+                                  'MinPeakDistance',in.min_interAP_interval*ISI,...
+                                  'MinPeakWidth',in.min_AP_width*exp_settings.sampling_rate);
+    peak_vals_all{i} = peak_vals;
+    peak_frames_all{i} = peak_frames;
+    if in.print_level > 0
+        fprintf('ROI %g: %g peaks found for %g stimuli\n',i,length(peak_vals),...
+                                                            length(stim_frames)); 
+    end
+end                          
+% Grab window around each peak
+mean_APs_all = cell(1,num_rois);
+deltaF_F0_all = cell(1,num_rois);
+for j = 1:num_rois
+    AP_framesj = nan(numAPframes,length(peak_frames)); % [num of stim frames x num of APs]
+    for i = 1:length(peak_frames)
+        stim_framei = find(frames_post_stim == peak_frames(i));
+        AP_framesi=(stim_framei-(numAPframes-1)/2):(stim_framei+(numAPframes-1)/2);
+        AP_framesi(AP_framesi<0) = nan; % exclude frames before stim
+        AP_framesi(AP_framesi>length(means_post_stim)) = nan; % exclude frames after end of recording
+        include_inds = ~isnan(AP_framesi) & AP_framesi > 0; % frames to include for this AP
+        AP_framesj(include_inds,i) = means_post_stim(AP_framesi(include_inds),j);          
+    end
+    mean_APs_all{j} = AP_framesj; 
+    deltaF_F0_all{j} = (AP_framesj - baselines(:,j)')./baselines(:,j)'; % get DF/F using AP-specific baseline (for jth roi)
+end
+
+% AP_winds(:,end) = [];
+tAP = 1e3*(1:numAPframes)/exp_settings.sampling_rate; % convert to ms
+tAP = tAP - tAP((length(tAP)+1)/2); % center around peak
+t = 1e3*(frames - 1)/exp_settings.sampling_rate; 
+% Plot
+cols = {'k','r'};
+light_cols = {0.4*ones(1,3),0.4*[1 0 0]};
+figure; 
+for i = 1:num_rois
+    coli = cols{i}; 
+    light_coli = light_cols{i};
+    ax1 = subplot(2,1,1);
+    plot(ax1,t,means(:,i),'Color',coli); hold on;
+    plot(ax1,1e3*peak_frames_all{i}/exp_settings.sampling_rate,peak_vals_all{i},'o','Color',light_coli);
+    ylabel(ax1,'Mean F (a.u.)');     
+    box(ax1,'off');
+    ax1.FontSize = in.font_size;
+%     ax1.XLim = [frames(1),frames(end)];
+%     xlabel(ax1,'Frames'); 
+    ax1.XLim = [0.9*1e3*stim_frames(1)/exp_settings.sampling_rate t(end)]; 
+    ax2 = subplot(2,1,2);
+    plot(ax2,tAP,deltaF_F0_all{in.plot_roi_ind},'Color',light_cols{in.plot_roi_ind},'LineWidth',0.5,'Marker','.'); hold on;
+    plot(ax2,tAP,mean(deltaF_F0_all{in.plot_roi_ind},2,'omitnan'),cols{in.plot_roi_ind},'LineWidth',2,'Marker','.'); 
+    ylabel(ax2,'\DeltaF/F_{0}'); 
+    box(ax2,'off');     
+    ax2.FontSize = in.font_size;
+    ax2.XLim = [tAP(1), tAP(end)]; 
+    xlabel(ax2,'Time (ms)'); 
+end
+plot(ax1,1e3*stim_frames/exp_settings.sampling_rate,ax1.YLim(2)*0.99*ones(1,length(stim_frames)),...
+    'r.','MarkerSize',8,'DisplayName','Stim times'); 
