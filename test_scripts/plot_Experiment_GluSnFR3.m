1e8b024da54fe10a4b38ad4b019d77ca01a7a468
Aman Aberra <aaberra0@gmail.com>
2021-10-14 10:56:11 -0400

    Add script combining all plotTrials scripts for GluSnFR3 DTX experiments

---
+++ test_scripts/plot_Experiment_GluSnFR3.m
@@ -0,0 +1,56 @@
+%% Script to plot single trial
+% Overlay single trails on same figure by running with different img_name
+data_fold = getDataFold();
+exp_date= '20211005';
+reporter = 'GluSnFR3';
+dish = 'dish1';
+condition = 'control'; % 'control', '5nM_DTX', '50nM_DTX'
+position = 'PosX'; 
+img_name = 'control.fits'; 
+stim_vals = 3; % frames - 3 sec delay (100 Hz sampling time)
+stim_wind = 0.5; % window
+baseline_wind = 0.15; % frames before stim/s to take baseline
+units = 'sec'; % specify units 'frames' or 'sec' 
+sampling_rate = 200; % sampling rate (frames/sec)
+exp_settings = ExperimentSettings(stim_vals,stim_wind,baseline_wind,...
+                                  units,sampling_rate); % automatically converts to frames
+roi_set_filename = 'RoiSet_pc_posX.zip'; 
+% Optional settings
+plot_settings = struct();
+plot_settings.show_diff_image = [3]; % can include [1,2,3]
+plot_settings.filt_width = 0;
+plot_settings.funcs = {'baseline','deltaF_F0'};
+plot_settings.roi_func_mode = 'combine';
+plot_settings.save_processed_data = 1;
+plot_settings.load_processed_data = 0;
+plot_settings.save_fig = 0;
+plot_settings.y_lim = [-0.2 1.4];
+plot_settings.plot_func = 'deltaF_F0'; % 'deltaF_F0'
+%%
+trace_fig = figure; 
+trace_axis = gca;
+datai = plotTrial(data_fold,exp_date,reporter,dish,condition,position,...
+                   img_name,exp_settings,roi_set_filename,...
+                   trace_axis,plot_settings);
+%% Plot multiple trials, same condition
+condition = 'control'; % 'control', '5nM_DTX', '50nM_DTX'
+img_names = {}; % use all images in condition folder
+[deltaF_F0,peaks_deltaF_F0,mean_peak_deltaF_F0,std_peak_deltaF_F0,trial_times,bslines] = ...
+        plotTrials(data_fold,exp_date,reporter,dish,condition,position,img_names,...
+                exp_settings,roi_set_filename,plot_settings);
+%% Plot multiple trials, multiple conditions       
+conditions = {'control','1nM_DTX'}; 
+% conditions = {'control','1nM_DTX','5nM_DTX','50nM_DTX','100nM_DTX','wash'}; % ,'wash'             
+positions = repmat({position},1,length(conditions));
+img_names = cell(1,length(conditions)); % use all images in condition folder
+roi_set_filenames = [repmat({roi_set_filename},1,length(conditions))];
+out = plotTrials_multipleConditions(data_fold,exp_date,reporter,dish,...
+                               conditions,positions,img_names,exp_settings,...
+                               roi_set_filenames,...
+                               plot_settings);
+%% Plot summary data
+plot_inds = [1,2,3,4];
+plotDefaultSummaryStats(out.peaks_deltaF_F0_all,out.poststim_ints_all,out.peak_times_all,...
+                         out.baselines_all,out.rel_times_cond_starts,...
+                         conditions,exp_date,reporter,dish,plot_settings,...
+                         summary_fig_dir,'plot_inds',plot_inds)   
