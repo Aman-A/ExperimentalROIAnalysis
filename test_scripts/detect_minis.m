b2238f8b0457bc489b1c7adab91a9374c2fc43d7
DESKTOP-K460EDQ\aaber <aaberra0@gmail.com>
2021-11-17 18:21:12 -0500

    Add work in progress test scripts for mini detection

---
+++ test_scripts/detect_minis.m
@@ -0,0 +1,70 @@
+function mini_frames = detect_minis(F,settings,method,varargin)
+% DETECT_MINIS
+% F - matrix
+%   mean fluorescence traces in each ROI [num_time_points x num_rois]
+% settings - struct
+%   structure with algorithm settings
+%   for method 1:
+%       nframes_back
+%       nframes_forward
+%       roi_with_mini_index
+%       blank_around_stim
+in.plot_figs = 1;
+in = sl.in.processVarargin(in,varargin);
+
+switch method
+    case 1 % Simple method using threshold based on noise level (std)
+        sampling_rate = settings.sampling_rate;
+        nframes_back = settings.nframes_back;
+        nframes_forward = settings.nframes_forward; 
+        roi_with_mini_index = settings.roi_with_mini_index;
+        stim_frame = settings.stim_frame;
+        blank_around_stim = settings.blank_around_stim; 
+        threshold = settings.threshold; 
+        min_mini_width = settings.min_mini_width; 
+        % get noise level
+        F_blank = F;
+        F_blank((stim_frame-blank_around_stim):(stim_frame+blank_around_stim),:) = nan; % blank out frames around stimulus
+        F_std = std(F_blank,0,1,'omitnan'); % std of signal in each ROI
+        thresholds = threshold*F_std; 
+        num_rois = size(F,2); 
+        mini_frames = cell(num_rois,1); 
+        mini_traces = cell(1,num_rois); 
+        roi_inds = []; % ROI index for each mini
+        for i = 1:num_rois
+            if any(F_blank(:,i) >= thresholds(i))
+                [mini_peak,mini_frames{i}] = findpeaks(F_blank(:,i),...
+                                          'MinPeakHeight',thresholds(i),...
+                                          'MinPeakWidth',min_mini_width*sampling_rate,...
+                                          'MinPeakDistance',2*min_mini_width*sampling_rate); %,...
+%                                           'WidthReference','halfheight'); 
+                roi_inds = [roi_inds;i*ones(length(mini_frames{i}),1)];
+                mini_traces{i} = nan(nframes_back+nframes_forward+1,length(mini_frames{i}));
+                for j = 1:length(mini_frames{i})
+                    mini_framesij = (mini_frames{i}(j)-nframes_back):(mini_frames{i}(j)+nframes_forward);
+                    mini_framesij(mini_framesij<=0) = nan; % exclude frames outside of recording window
+                    mini_framesij(mini_framesij>size(F,1)) = nan;
+                    include_inds = ~isnan(mini_framesij); % frames to include for this mini
+                    mini_traces{i}(include_inds,j) = F(mini_framesij(include_inds),i);
+                end        
+            else
+               mini_frames{i} = [];  
+            end
+        end
+        num_rois_w_mini = sum(~cellfun(@isempty,mini_frames,'UniformOutput',1)); 
+        num_minis = sum(cellfun(@length,mini_frames,'UniformOutput',1)); 
+        fprintf('Found %g minis from %g ROIs (%g total ROIs)\n',num_minis,...
+                num_rois_w_mini,num_rois); 
+end
+aligned_minis = cell2mat(mini_traces(~cellfun(@isempty,mini_traces)));
+mini_frames_lin = cell2mat(mini_frames); % convert to vector
+% Convert to deltaF/F0
+baselines = mean(aligned_minis(1:nframes_back,:),1);
+mini_deltaF_F_traces = aligned_minis./abs(baselines); % divide each column by corresponding baseline
+if in.plot_figs
+   t = 0:(1/sampling_rate):(size(mini_deltaF_F_traces,1)-1)/sampling_rate;
+   figure; 
+   plot(t*1e3,mini_deltaF_F_traces); 
+   xlabel('time (ms)'); ylabel('\Delta F/F_{0}'); 
+end
+end
---
+++ test_scripts/test_mini_detection.m
@@ -0,0 +1,59 @@
+%% Mini detection 
+% Analysis settings
+method = 1; % mini finding method
+threshold = 3.5; % x std (noise level)
+min_mini_width = 20e-3; % sec - min FWHM of minis
+apply_high_pass_filt = 1;    
+fc = 1/3; % Hz - filter cutoff
+nframes_back = 10;  % number of frames behind each mini to extend window
+nframes_forward = 10; 
+roi_with_mini_index = 25; % index of ROI with known mini
+% Path to data and ROIs
+exp_folder = fullfile(getDataFold(),'../Sam_data/20210908 d5'); 
+img_folder = fullfile(exp_folder,'the stack');
+roi_set_name = 'RoiSet_analysis';
+% Experiment/imaging settings
+sampling_rate = 100; % 
+stim_vals = 100; 
+stim_wind = 10; 
+baseline_wind = 10; 
+units = 'frames';
+exp_settings = ExperimentSettings(stim_vals,stim_wind,baseline_wind,units,sampling_rate); 
+%% Load recording
+img_names = getImagesWithinDir(img_folder); 
+img_name = img_names{3}; 
+rec = Recording(fullfile(img_folder,img_name));  
+rec.load(); 
+rois = ROIs(fullfile(exp_folder,roi_set_name)); 
+%% Extract traces in ROIs
+func_output = calcROIfuncs(rec,rois,{'mean','deltaF_F0'},exp_settings.baseline_wind_inds,...
+                            'separate');
+deltaF_F0 = func_output.deltaF_F0;    
+means = func_output.mean;
+%% High pass filter to remove photobleaching transient
+if apply_high_pass_filt
+    filt_order = 3; 
+    [b,a] = butter(filt_order,fc/(exp_settings.sampling_rate/2),'high');
+    deltaF_F0 = filtfilt(b,a,deltaF_F0); 
+    means = filtfilt(b,a,means); 
+    fprintf('Applied %g order high pass filter with %g Hz cutoff\n',filt_order,fc); 
+else
+    fprintf('Skipped filtering step\n'); 
+end
+%% Plot traces in all
+fig = figure('Units','inches','Position',[0.4479    1.3021   19.0625    8.9583]); 
+ax = gca;
+plotROIfunc(func_output,'deltaF_F0',exp_settings.stim_vals,...
+            exp_settings.sampling_rate,'ax',ax,...
+            'show_legend',0);
+%% 
+settings.sampling_rate = exp_settings.sampling_rate;
+settings.nframes_back = nframes_back;
+settings.nframes_forward = nframes_forward; 
+settings.stim_frame = exp_settings.stim_vals;
+settings.blank_around_stim = exp_settings.stim_wind; 
+settings.threshold = threshold;
+settings.roi_with_mini_index = roi_with_mini_index; 
+settings.min_mini_width = min_mini_width;
+mini_times = detect_minis(means,settings,method); 
+
