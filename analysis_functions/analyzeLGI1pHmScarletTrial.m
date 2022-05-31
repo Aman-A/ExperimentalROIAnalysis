a4a67a4a3fe73e4bbf1faa68e777080643dadb91
Aman Aberra <aaberra0@gmail.com>
2022-05-31 10:58:16 -0400

    Finish LGI1pHmScarlet trial plotting/analysis function

--- analysis_functions/analyzeLGI1pHmScarletTrial.m
+++ analysis_functions/analyzeLGI1pHmScarletTrial.m
@@ -1,38 +1,75 @@
-function analyzeLGI1pHmScarletTrial(varargin)
+function lgi1_data = analyzeLGI1pHmScarletTrial(img_name,exp_settings,roiset_filename,...
+                                    background_roiset_filename,varargin)
 
-psi_lgi1 = psi;
-psi_lgi1.condition = in.lgi1_condition;
-if ~isempty(psi_main.show_diff_image)
-    psi_lgi1.show_diff_image = 1; % always show baseline
-else
-    psi_lgi1.show_diff_image = [];
+in = plotTrialSettings('plot_func','none','show_diff_image',1); 
+in = sl.in.processVarargin(in,varargin);
+%
+lgi1_rois = plotTrial(img_name,exp_settings,roiset_filename,[],in);
+
+if isempty(background_roiset_filename)
+    background_roiset_filename = [roiset_filename '_background']; 
 end
-psi_lgi1.plot_func = 'none'; % default skip plotting
-lgi1_rois = plotTrial(def.lgi1_trial_name{i},in.lgi1_exp_settings,...
-                    roiset_filenamei,[],psi_lgi1);
-dish_foldi = fullfile(psi.data_fold,psi.exp_date,psi.reporter,...
-                    psi.dish);
-background_roiset = [roiset_filenamei '_background'];        
-background_rois_filepath = fullfile(dish_foldi,background_roiset);
-if exist([background_rois_filepath '.zip'],'file')
-    lgi1_bg =  plotTrial(def.lgi1_trial_name{i},in.lgi1_exp_settings,...
-                        background_roiset,[],psi_lgi1);
-else
-    lgi1_bg = []; 
-    fprintf('WARNING: No background ROI for LGI1 normalization for %s/%s\n',...
-            psi.exp_date,psi.dish)
+
+lgi1_bg =  plotTrial(img_name,exp_settings,background_roiset_filename,[],in);
+
+if in.save_processed_data
+    % Save
+    dish_fold = fullfile(in.data_fold,in.exp_date,in.reporter,in.dish);
+    lgi1_data = struct('data',lgi1_rois,'background',lgi1_bg,'plot_settings',in);
+    roiset_filename_no_exti = getROIset_name(roiset_filename,...
+                                             in.transform_type,...
+                                             in.registration_rec);  
+    lgi1_datafile = sprintf('%s_%s_%s_%s_%s_%s_%s',in.exp_date,...
+                                in.reporter,in.dish,...
+                                in.roi_func_mode,...
+                                roiset_filename_no_exti,...
+                                in.condition,img_name);       
+    lgi1_datafilepath = fullfile(dish_fold,lgi1_datafile);            
+    save(lgi1_datafilepath,'-STRUCT','lgi1_data');
+    fprintf('Saved LGI1 data to %s\n',lgi1_datafile);
 end
-% Save
-lgi1_data = struct('data',lgi1_rois,'background',lgi1_bg);
-roiset_filename_no_exti = getROIset_name(roiset_filenamei,...
-                                             psi.transform_type,...
-                                             psi.registration_rec);  
-lgi1_datafile = sprintf('%s_%s_%s_%s_%s_%s_%s',psi.exp_date,...
-                            psi.reporter,psi.dish,...
-                            psi.roi_func_mode,...
-                            roiset_filename_no_exti,...
-                            in.lgi1_condition,def.lgi1_trial_name{i});       
-lgi1_datafilepath = fullfile(dish_foldi,lgi1_datafile);            
-save(lgi1_datafilepath,'-STRUCT','lgi1_data');
-fprintf('Saved LGI1 data to %s\n',lgi1_datafile);
+if ~isempty(in.show_diff_image) && all(in.show_diff_image ~= 0)
+   % Plot settings
+    in.cmap = 'inferno';
+    in.formats = {'png'};
+    in.resolutions = {'-r300'};
+    in.fig_settings = {'Units','normalized','Position',[0 0.1667 0.75 0.744],...
+        'Color','k'};
+    in.cb_settings = {'Color','w','FontSize',14};
+    in.title_settings = {'Interpreter','none','Color','w'};
+    in.cax_mode = 'quantile'; % 'quantile', 'abs', or 'auto'
+    in.cax_lims = [0.02 0.998]; % color limits, units defined in cax_mode
+    % Plot
+    normF_img = lgi1_rois.mean_bsline_img/mean(lgi1_bg.func_output.mean);
+    fig = figure(in.fig_settings{:});
+    imagesc(normF_img)
+    ax = gca;
+    axis(ax,'equal','off'); hold(ax,'on');
+    colormap(ax,in.cmap);
+    colorbar(ax, in.cb_settings{:});
+    axis(ax,[0 size(normF_img,2) 0 size(normF_img,1)]);
+    title(ax,'LGI1 fluorescence normalized to background',in.title_settings{:});
+    if strcmp(in.cax_mode,'quantile')
+        caxis(ax,quantile(normF_img(:),in.cax_lims))
+    elseif strcmp(in.cax_mode,'abs')
+        caxis(ax,in.cax_lims);
+    end % if cax_mode 'auto', leave as is
+    if ~isempty(in.pixel_size)
+        % get integer length (µm) and convert back to pixels
+        addScaleBar(in.pixel_size,size(normF_img),ax,...
+            'Color',in.cb_settings{find(strcmp('Color',in.cb_settings))+1})
+    end
+    ax.YDir = 'normal';
+    lgi1_rois.rois.plot('g',ax,1,1);
+    lgi1_bg.rois.plot('w',ax,1,1);
+    if in.save_fig
+        roiset_filename_no_ext = getROIset_name(roiset_filename,...
+            in.transform_type,...
+            in.registration_rec);
+        printFig(fig,fullfile(in.data_fold,in.exp_date,in.reporter,in.dish,...
+            in.condition,['figs_' roiset_filename_no_ext]),...
+            [lgi1_rois.recording.img_name '_' 'normbg'],...
+            'formats',in.formats,'resolutions',in.resolutions);
+    end
+end
 end
---
+++ experiment_scripts/plot_Experiment_LGI1pHmScarlet.m
@@ -0,0 +1,36 @@
+%% Script to plot single trial
+% Overlay single trials on same figure by running with different img_name
+% 1kHz recording, 2 x 2 binning (half res of number of pixels)
+data_fold = getDataFold();
+exp_date= '20220525';
+reporter = 'Archon_LGI1pHmScarlet';
+dish = 'dish4';
+div = 15; 
+roiset_filename = 'RoiSet_pos5';
+lgi1_exp_settings = ExperimentSettings(51,-1,50,'frames',2e3); % dummy frame rate                                 
+% Optional settings
+ps = plotTrialSettings;
+ps.data_fold = data_fold;
+ps.exp_date = exp_date;
+ps.reporter = reporter;
+ps.dish = dish;
+ps.div = div; 
+ps.show_diff_image = [1]; % can include [1,2,3]
+ps.filt_width = 0;
+ps.funcs = {'mean'};
+ps.roi_func_mode = 'combine';
+ps.save_processed_data = 1;
+ps.load_processed_data = 1;
+ps.save_fig = 1;
+ps.plot_func = 'none'; % 'deltaF_F0_aligned' 'deltaF_F0'
+ps.show_roi_labels = 1;
+ps.close_img_after_save = 0; 
+%%
+ps.condition = 'lgi1_pHmScarlet';
+img_name = 'trial_2';
+trace_fig = figure;
+trace_axis = gca;
+lgi1_data = analyzeLGI1pHmScarletTrial(img_name,...
+                        lgi1_exp_settings,roiset_filename,[],ps);                        
+
+               
