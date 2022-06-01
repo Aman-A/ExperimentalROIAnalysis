b6dcb54310893aa3c864cefc12ba2f69c6c7e2b1
DESKTOP-K460EDQ\aaber <aaberra0@gmail.com>
2022-06-01 17:13:48 -0400

    Use reporter as input argument of loadDataset

--- util_functions/load_dataset_functions/loadDataset.m
+++ util_functions/load_dataset_functions/loadDataset.m
@@ -1,4 +1,4 @@
-function [data,def,varargout] = loadDataset(dataset_def_filename,...
+function [data,def,varargout] = loadDataset(dataset_def_filename,reporter,...
                                            roi_func_mode,mode_str,varargin)
 %LOADDATASET Compiles data from multiple dishes defined in dataset
 %definition file and loads, helper function for reporter specific
@@ -18,21 +18,21 @@ function [data,def,varargout] = loadDataset(dataset_def_filename,...
 if nargin < 3 || isempty(mode_str)
    mode_str = 'default';
 end
-def = loadDatasetDefinition(dataset_def_filename);
-if all(strcmp(def.reporter,'GluSnFR3'))
-    [data,~,norm_data,train_data] = loadDefaultDataset(dataset_def_filename,...
+% def = loadDatasetDefinition(dataset_def_filename);
+if strcmp(reporter,'GluSnFR3')
+    [data,def,norm_data,train_data] = loadDefaultDataset(dataset_def_filename,...
                                                    roi_func_mode,varargin{:});
     varargout = {norm_data,train_data};    
-elseif ~isempty(regexp(def.reporter{1},'Archon','ONCE'))
-    if all(strcmp(def.reporter,'Archon')) && strcmp(mode_str,'default')
-        [data,~,norm_data,train_data] = loadDefaultDataset(dataset_def_filename,...
+elseif ~isempty(regexp(reporter,'Archon','ONCE'))
+    if strcmp(reporter,'Archon') && strcmp(mode_str,'default')
+        [data,def,norm_data,train_data] = loadDefaultDataset(dataset_def_filename,...
                                                    roi_func_mode,varargin{:});
         varargout = {norm_data,train_data};
     else % for paired pulse experiments with or without LGIphmScarlet indicator
-        [data,~,AP_data,lgi1_data] = loadArchonLGI1pHmScarletDataset(dataset_def_filename,...
+        [data,def,AP_data,lgi1_data] = loadArchonLGI1pHmScarletDataset(dataset_def_filename,...
                                                        roi_func_mode,varargin{:});
         varargout = {AP_data,lgi1_data};
     end
 else
-    error('Not implemented for input reporter: %s',def.reporter{1});
+    error('Not implemented for input reporter: %s',reporter);
 end
