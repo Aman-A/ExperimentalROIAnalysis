f79a305b0083fe04a1833fe0fad01e5cdf2cdcd2
DESKTOP-K460EDQ\aaber <aaberra0@gmail.com>
2022-10-06 18:17:12 -0400

    Modify ExperimentSettings class to make num_stim and num_trains Dependent properties, allows modifying instantiated object. Add get function for stim_wind_inds to update in case stim_vals changed. Modify Recording class to load image size without loading file. calcROIfuncs truncates stim if stim_window larger than recording length after last stim

--- ExperimentSettings.m
+++ ExperimentSettings.m
@@ -1,35 +1,40 @@
-classdef ExperimentSettings < handle % Stimulus, recording, and analysis settings for imaging
+classdef ExperimentSettings < handle & matlab.mixin.Copyable % Stimulus, recording, and analysis settings for imaging
                   % experiment
     properties
         stim_vals {mustBeNumeric} % N stimuli x M trains array of stimulus times 
-                                  % single train is 1 x N vector
+                                  % single train is 1 x N vector, stays in
+                                  % same units as input
         stim_wind {mustBeNumeric} % time window to extract post-stimulus statistics, e.g. peak,
                                   %  generates NxM windows
         baseline_wind {mustBeNumeric} % time window to extract baseline, either
-                                      % 1 window or N windows (for N stimuli)
-        units char {mustBeTextScalar} % string, units of input stim_vals, 
-                                      % stim_wind, and baseline_wind, 
-                                      % either 'frames' or 'sec'
+                                      % 1 window or N windows (for N stimuli)        
         sampling_rate {mustBeNumeric} % sampling rate in frames/sec
         stim_pulse_dur {mustBeNumeric} % Stimulus pulse duration/s, scalar 
                                        % for uniform duration, or array same
                                        % size as stim_vals with duration of
-                                       % each stimulus                                
+                                       % each stimulus                                        
+        baseline_start_frame = 1; % start baseline from 1 frame before 
+                                  % stimulus frames               
+    end
+    properties (Dependent)
+        num_stim  % number of stimuli within train
+        num_trains  % number of repeated stimulus trains within stim_vals,
+                            % if stim_vals is 1D vector, considered 1 pulse
+                            % train
+                            % if stim_vals is 2D, each row of stim times is
+                            % treated as a separate stim train in subsequent
+                            % analysis                                       
+    end
+    properties (SetAccess = private)
         stim_wind_inds = []; % stimulus windows as either single column vector 
                        % (1 stimulus) or stim_wind x N stimuli x
                        % M trains array
-        baseline_wind_inds = [];% baseline windows as either single column vector 
+        baseline_wind_inds = []; % baseline windows as either single column vector 
                            % (1 stimulus) or stim_wind x N stimuli x M
-                           % trains array
-        baseline_start_frame = 1; % start baseline from 1 frame before 
-                                  % stimulus frames
-        num_stim = []; % number of stimuli within train
-        num_trains = []; % number of repeated stimulus trains within stim_vals,
-                         % if stim_vals is 1D vector, considered 1 pulse
-                         % train
-                         % if stim_vals is 2D, each row of stim times is 
-                         % treated as a separate stim train in subsequent 
-                         % analysis
+                           % trains array    
+%         initial_units; % units of stim_vals when object was instantiated
+        units  % string, current units of stim_wind, and baseline_wind, 
+               % either 'frames' or 'sec'  
     end
     methods
         function obj = ExperimentSettings(stim_vals,stim_wind,baseline_wind,units,...
@@ -38,19 +43,19 @@ classdef ExperimentSettings < handle % Stimulus, recording, and analysis setting
               in.stim_pulse_dur = [];
               in = sl.in.processVarargin(in,varargin); 
             if nargin > 0
-                obj.stim_vals = stim_vals;
-                obj.stim_wind = stim_wind;
-                obj.baseline_wind = baseline_wind;
+%                 obj.initial_units = units; 
                 obj.units = units;
-                obj.sampling_rate = sampling_rate;                        
-                obj.num_trains = size(obj.stim_vals,1);
-                obj.num_stim = size(obj.stim_vals,2);
+                obj.sampling_rate = sampling_rate;                                
+                obj.stim_wind = stim_wind;
+                obj.baseline_wind = baseline_wind;                                                        
                 obj.stim_pulse_dur = in.stim_pulse_dur; 
-            end            
-            convert2Frames(obj);  
-            getWindInds(obj);            
+                obj.stim_vals = stim_vals; % set other properties first so get method can update stim_wind_inds/baseline_inds
+                obj.convert2Frames()
+            end                    
         end
-        function varargout = convert2Frames(obj,varargin)            
+        function varargout = convert2Frames(obj,varargin)     
+            % Convert properties or input vector of time points to frame
+            % numbers
             if nargin == 1
                 if strcmp(obj.units,'sec')
                     obj.stim_vals = round(obj.stim_vals*obj.sampling_rate); % round to nearest frame
@@ -79,37 +84,18 @@ classdef ExperimentSettings < handle % Stimulus, recording, and analysis setting
             elseif nargin == 2 % just convert input value/s
                 varargout = {frames_val/obj.sampling_rate}; 
             end
+        end             
+        function num_trains = get.num_trains(obj)
+           num_trains = size(obj.stim_vals,1);                 
         end
-        function getWindInds(obj)                        
-            % add window indices
-            if isempty(obj.stim_vals) % no stim, use initial frames as 
-                                      % default baseline otherwise, 
-                                      % calculate on per event basis 
-                                      % externally
-                obj.baseline_wind_inds = (1:obj.baseline_wind)'; 
-            else
-                if obj.baseline_wind + obj.baseline_start_frame > obj.stim_vals(1)
-                    fprintf(['Warning baseline_wind of %g exceeds pre-stimulus ',...
-                        'recording time, setting to max possible %g frames\n'],...
-                        obj.baseline_wind,obj.stim_vals(1) - obj.baseline_start_frame);
-                    obj.baseline_wind = obj.stim_vals(1) - obj.baseline_start_frame ;
-                end
-                if numel(obj.stim_vals) > 1                
-                    obj.stim_wind_inds = zeros(obj.stim_wind,obj.num_stim,obj.num_trains);  % [stim_wind x num_stim x num_trains]
-                    obj.baseline_wind_inds = zeros(obj.baseline_wind,obj.num_stim,obj.num_trains);
-                    for i = 1:obj.num_trains
-                        for j = 1:obj.num_stim
-                            obj.stim_wind_inds(:,j,i) = (obj.stim_vals(i,j)+1):(obj.stim_vals(i,j) + obj.stim_wind);
-                            obj.baseline_wind_inds(:,j,i) = ...
-                             (obj.stim_vals(i,j) - obj.baseline_wind - obj.baseline_start_frame + 1):(obj.stim_vals(i,j) - obj.baseline_start_frame);
-                        end
-                    end
-                else
-                    obj.stim_wind_inds = ((obj.stim_vals + 1):(obj.stim_vals + obj.stim_wind + 1))';
-                    obj.baseline_wind_inds = ...
-                     ((obj.stim_vals - obj.baseline_wind - obj.baseline_start_frame + 1):(obj.stim_vals - obj.baseline_start_frame))';
-                end            
-            end
+        function num_stim = get.num_stim(obj)
+           num_stim = size(obj.stim_vals,2); 
+        end       
+        function stim_wind_inds = get.stim_wind_inds(obj)
+            [stim_wind_inds,~] = obj.getWindInds();
+        end
+        function baseline_wind_inds = get.baseline_wind_inds(obj)
+            [~,baseline_wind_inds] = obj.getWindInds();
         end
         function t = getTimeVector(obj,num_frames)            
             x = (1:num_frames)';
@@ -122,6 +108,45 @@ classdef ExperimentSettings < handle % Stimulus, recording, and analysis setting
 %                 end
 %                 t = t - stim_time;
 %             end
+        end
+    end
+    methods (Access = private)
+        function [stim_wind_inds,baseline_wind_inds] = getWindInds(obj)
+            obj.convert2Frames()
+            % add window indices
+            if isempty(obj.stim_vals) % no stim, use initial frames as
+                % default baseline otherwise,
+                % calculate on per event basis
+                % externally
+                stim_wind_inds = []; 
+                baseline_wind_inds = (1:obj.baseline_wind)';
+            else
+                if obj.baseline_wind + obj.baseline_start_frame > obj.stim_vals(1)
+                    fprintf(['Warning baseline_wind of %g exceeds pre-stimulus ',...
+                        'recording time, setting to max possible %g frames\n'],...
+                        obj.baseline_wind,obj.stim_vals(1) - obj.baseline_start_frame);
+                    obj.baseline_wind = obj.stim_vals(1) - obj.baseline_start_frame ;
+                end
+                if numel(obj.stim_vals) > 1
+                    stim_wind_inds = zeros(obj.stim_wind,obj.num_stim,... % [stim_wind x num_stim x num_trains]
+                                              obj.num_trains);  
+                    baseline_wind_inds = zeros(obj.baseline_wind,...
+                                              obj.num_stim,obj.num_trains);
+                    for i = 1:obj.num_trains
+                        for j = 1:obj.num_stim
+                            stim_wind_inds(:,j,i) = ...
+                                (obj.stim_vals(i,j)+1):(obj.stim_vals(i,j) + obj.stim_wind);
+                            baseline_wind_inds(:,j,i) = ...
+                                (obj.stim_vals(i,j) - obj.baseline_wind - obj.baseline_start_frame + 1):(obj.stim_vals(i,j) - obj.baseline_start_frame);
+                        end
+                    end
+                else
+                    stim_wind_inds = ...
+                        ((obj.stim_vals + 1):(obj.stim_vals + obj.stim_wind + 1))';
+                    baseline_wind_inds = ...
+                        ((obj.stim_vals - obj.baseline_wind - obj.baseline_start_frame + 1):(obj.stim_vals - obj.baseline_start_frame))';
+                end
+            end
         end
     end
     
