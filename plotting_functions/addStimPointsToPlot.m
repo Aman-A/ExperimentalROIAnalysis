1a291f8deee31ad426de7f2c195ed16b2081b99c
DESKTOP-K460EDQ\aaber <aaberra0@gmail.com>
2022-11-21 19:43:30 -0500

    Fix plot setting bug in addStimPointsToPlot

--- plotting_functions/addStimPointsToPlot.m
+++ plotting_functions/addStimPointsToPlot.m
@@ -19,7 +19,7 @@ function stimpoints_hand = addStimPointsToPlot(stim_frames,stim_marker_mode,ax,.
 in.stim_pulse_dur = []; % duration of stimuli (same units as plot's time base), 
                   % should be vector same length as stim_frames
 in.stim_leg_name = '';
-in.color = 'r';
+in.color = 'k';
 in = sl.in.processVarargin(in,varargin);
 if nargin < 3
     ax = gca;
@@ -47,7 +47,7 @@ ind_stim_times = strcmp(leg_names,stim_leg_name);
 if ~any(ind_stim_times) % only plot if stim times don't already exist on this axis
     if stim_marker_mode == 1
         stimpoints_hand = plot(ax,stim_frames,ax.YLim(2)*0.99*ones(1,length(stim_frames)),...
-            'Color',in.color,'Marker','.','MarkerSize',8,'DisplayName',stim_leg_name);
+            'LineStyle','none','Color',in.color,'Marker','.','MarkerSize',8,'DisplayName',stim_leg_name);
     elseif stim_marker_mode == 2
         stimpoints_hand = plot(ax,[stim_frames(:)';stim_frames(:)';nan(size(stim_frames(:)'))],...
             [ax.YLim'.*[1;0.99].*ones(2,length(stim_frames(:)'));nan(size(stim_frames(:)'))],...
