2b57cce58772b6e40ba4ed2de33e61924ba1730c
Aman Aberra <aaberra0@gmail.com>
2023-04-25 18:05:11 -0400

    Add exclude formats option to getImagesWithinDir, use loop

--- util_functions/getImagesWithinDir.m
+++ util_functions/getImagesWithinDir.m
@@ -1,4 +1,4 @@
-function img_names = getImagesWithinDir(filedir,ignore_mot_corr)
+function img_names = getImagesWithinDir(filedir,ignore_mot_corr,exclude_formats)
 %GETIMAGESWITHINDIR Gets images within directory sorted by time of creation
 %  
 %   Inputs 
@@ -15,19 +15,29 @@ if nargin < 2
     ignore_mot_corr = 1; % ignore files with _motcorr_ in name, typically 
                          % corresponds to motion corrected recordings in same folder
 end
+if nargin < 3
+    exclude_formats = {}; 
+end
 d = dir(filedir); 
 file_names = {d.name};
 creation_time = [d.datenum];
 [~,inds] = sort(creation_time,'ascend');
 file_names = file_names(inds);
+formats = {'.fits','.tif','.tiff','.nd2','.czi'};
+formats = setdiff(formats,exclude_formats); 
 [~,~,file_exts] = cellfun(@(x) fileparts(x),file_names,'UniformOutput',0);
-is_fits = cellfun(@(x) strcmp(x,'.fits'),file_exts,'UniformOutput',1);
-is_tif = cellfun(@(x) strcmp(x,'.tif'),file_exts,'UniformOutput',1);
-is_tiff = cellfun(@(x) strcmp(x,'.tiff'),file_exts,'UniformOutput',1);
-is_nd2 = cellfun(@(x) strcmp(x,'.nd2'),file_exts,'UniformOutput',1);
-is_czi = cellfun(@(x) strcmp(x,'.czi'),file_exts,'UniformOutput',1);
+is_img = false(1,length(file_names));
+for i = 1:length(formats)
+    is_img = is_img | cellfun(@(x) strcmp(x,formats{i}),file_exts,'UniformOutput',1);
+end
+% is_fits = cellfun(@(x) strcmp(x,'.fits'),file_exts,'UniformOutput',1);
+% is_tif = cellfun(@(x) strcmp(x,'.tif'),file_exts,'UniformOutput',1);
+% is_tiff = cellfun(@(x) strcmp(x,'.tiff'),file_exts,'UniformOutput',1);
+% is_nd2 = cellfun(@(x) strcmp(x,'.nd2'),file_exts,'UniformOutput',1);
+% is_czi = cellfun(@(x) strcmp(x,'.czi'),file_exts,'UniformOutput',1);
 is_not_hidden = cellfun(@(x) ~strcmp(x(1),'.'),file_names,'UniformOutput',1);
-img_names = file_names((is_fits | is_tif | is_tiff | is_nd2 | is_czi) & is_not_hidden);
+
+img_names = file_names(is_img & is_not_hidden);
 if ignore_mot_corr
     img_names = img_names(~contains(img_names,'_motcorr'));    
 end
