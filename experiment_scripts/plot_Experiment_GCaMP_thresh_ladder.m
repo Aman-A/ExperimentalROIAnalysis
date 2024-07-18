15163905eb223e515ca4da45a7c17a88cb04d2bc
Aman Aberra <aaberra0@gmail.com>
2024-07-17 23:44:54 -0400

    Bug fixes

--- experiment_scripts/plot_Experiment_GCaMP_thresh_ladder.m
+++ experiment_scripts/plot_Experiment_GCaMP_thresh_ladder.m
@@ -11,14 +11,14 @@ ps = plotTrialSettings;
 ps.spike_thresh = 8; % 6 x std
 ps.spike_window = 0.1; % sec
 
-pulse_dur_ms = 1; % pulse duration in ms  
+pulse_dur_ms = 0.05; % pulse duration in ms  
 E_per_mA = 68.524; % V/m per mA 
 % E_per_mA = 65.27;
 isolator_mA_per_V = 10; 
 
 trial_ind = 1; 
+E_recs_fold = sprintf('E_recs_%gms',pulse_dur_ms);
 ps.condition = sprintf('thresh_%gms',pulse_dur_ms); % 
-E_recs_fold = ps.condition; 
 
 stim_file = sprintf('%s/E_trial_%04d.h5',E_recs_fold,trial_ind);
 img_name = reindexFileNameForAndor('trial',trial_ind);
@@ -46,11 +46,11 @@ ps.reporter = reporter;
 ps.dish = dish;
 ps.div = div; 
 ps.analysis_funcs = {'peaks','peak_times','successful_spikes'};
-ps.show_diff_image = [4]; % can include [1,2,3]
+ps.show_diff_image = []; % can include [1,2,3]
 ps.roi_func_mode = 'combine'; % 'combine' or 'separate'
 ps.save_processed_data = 1;
-ps.load_processed_data = 1;
-ps.save_fig = 2;
+ps.load_processed_data = 0;
+ps.save_fig = 0;
 ps.plot_func = 'deltaF_F0'; % 'deltaF_F0'  
 ps.blank_frame_inds = 1; 
 ps.rem_pbleach = 1;
@@ -71,38 +71,57 @@ plot(trace_axis,datai.func_output.trec(exp_settings.stim_vals(successful_spikes)
 amps = exp_settings.stim_amps; 
 above_thresh_amps = amps(successful_spikes);
 above_thresh_amps_inds = find(successful_spikes);
-threshE = above_thresh_amps(1)*exp_settings.E_per_mA;
-lower_lim_threshE = amps(above_thresh_amps_inds(1)-1)*exp_settings.E_per_mA;
-thresh_window_per_size = 100*(threshE - lower_lim_threshE)./lower_lim_threshE; 
-fprintf('Threshold for %g ms pulse between %.3f to %.3f V/m (%.2f %% window)\n',...
-        stim_pulse_dur*1e3,lower_lim_threshE,threshE,thresh_window_per_size)
+if isempty(above_thresh_amps)
+    fprintf('Threshold for %g ms pulse above %.3f V/m (%.3f mA)\n',...
+            exp_settings.stim_pulse_dur_sec*1e3,amps(end)*exp_settings.E_per_mA,amps(end))
+elseif above_thresh_amps_inds(1) == 1
+    fprintf('Threshold for %g ms pulse below %.3f V/m (%.3f mA)\n',...
+            exp_settings.stim_pulse_dur_sec*1e3,amps(1)*exp_settings.E_per_mA,amps(1))
+else
+    threshE = above_thresh_amps(1)*exp_settings.E_per_mA;
+    lower_lim_threshE = amps(above_thresh_amps_inds(1)-1)*exp_settings.E_per_mA;
+    thresh_window_per_size = 100*(threshE - lower_lim_threshE)./lower_lim_threshE; 
+    fprintf('Threshold for %g ms pulse between %.3f to %.3f V/m (%.2f %% window, %.3f mA)\n',...
+        exp_settings.stim_pulse_dur_sec*1e3,lower_lim_threshE,threshE,thresh_window_per_size,...
+        amps(above_thresh_amps_inds(1)))
+end
 %% Plot multiple trials, same condition
-ps.condition = sprintf('thresh_%gms',pulse_dur_ms); % 
-trial_inds = 1:5; 
-stim_files = numericVec2chars(trial_inds,sprintf('%s/E_trial_%%04d.h5',E_recs_fold));
-img_names = arrayfun(@(x) reindexFileNameForAndor('trial',x),trial_inds,'UniformOutput',0);
-% img_names = numericVec2chars(trial_inds-1,'1ms_ladder_%g');
-
-exp_settings_all = cellfun(@(x) WaveSurferStim2ExpSettings(x,sampling_rate,...
-                                           stim_wind,baseline_wind,'E_per_mA',...
-                                           E_per_mA),stim_files,'UniformOutput',1);
-
-% img_names = {}; % use all images in condition folder
-ps.load_processed_data = 1;
-ps.save_fig = 0;
-ps.show_diff_image = []; 
-ps.overlay_trials = 1; 
-trials_data = plotTrials(img_names,exp_settings,roiset_filename,ps);
-successful_spikes_all = squeeze(trials_data.analysis.successful_spikes);
-%% Plot threshold figs
-thresh_out = analyzeThresholdLadderTrials(exp_settings.stim_amps,successful_spikes_all,...
-                                'E_per_mA',E_per_mA,'plot_fig',1,...
-                                'save_fig',1,'fig_fold',fullfile(ps.condition,...
-                                                        ['figs_',trials_data.roiset_filename_no_ext]),...
-                                'save_data',1,'data_filename',...
-                                sprintf('thresh_data_%s',trials_data.roiset_filename_no_ext),...
-                                'data_fold',ps.condition);
-threshE = thresh_out.thresh;
+pulse_durs_ms_all = [0.05,0.1,0.2,0.5,1,5];
+% pulse_durs_ms_all = 0.05; 
+trial_inds_all = {1:5,1:5,1:5,1:5,1:5,1:8}; 
+threshEs = nan(length(pulse_durs_ms_all),1);
+for i = 1:length(pulse_durs_ms_all)
+    pulse_dur_ms = pulse_durs_ms_all(i); % pulse duration in ms     
+    ps.condition = sprintf('thresh_%gms',pulse_dur_ms); % 
+    E_recs_fold = sprintf('E_recs_%gms',pulse_dur_ms);
+    trial_inds = trial_inds_all{i}; 
+    stim_files = numericVec2chars(trial_inds,sprintf('%s/E_trial_%%04d.h5',E_recs_fold));    
+    img_names = arrayfun(@(x) reindexFileNameForAndor('trial',x),trial_inds,'UniformOutput',0);
+    % img_names = numericVec2chars(trial_inds-1,'1ms_ladder_%g');
+    
+    exp_settings_all = cellfun(@(x) WaveSurferStim2ExpSettings(x,sampling_rate,...
+                                               stim_wind,baseline_wind,'E_per_mA',...
+                                               E_per_mA,'isolator_mA_per_V',isolator_mA_per_V),...
+                                               stim_files,'UniformOutput',1);
+    
+    % img_names = {}; % use all images in condition folder
+    ps.load_processed_data = 0;
+    ps.save_fig = 0;
+    ps.show_diff_image = []; 
+    ps.overlay_trials = 1; 
+    trials_data = plotTrials(img_names,exp_settings_all,roiset_filename,ps);
+    successful_spikes_all = squeeze(trials_data.analysis.successful_spikes);
+    % Plot threshold figs
+    thresh_out = analyzeThresholdLadderTrials(exp_settings_all(1).stim_amps,successful_spikes_all,...
+                                    'E_per_mA',E_per_mA,'plot_fig',1,...
+                                    'save_fig',1,'fig_fold',fullfile(ps.condition,...
+                                                            ['figs_',trials_data.roiset_filename_no_ext]),...
+                                    'save_data',1,'data_filename',...
+                                    sprintf('thresh_data_%s',trials_data.roiset_filename_no_ext),...
+                                    'data_fold',ps.condition);
+    threshE = thresh_out.thresh;
+    threshEs(i) = threshE;
+end
 % fit to logistic regression
 % amps_all = cell2mat(arrayfun(@(x) x.stim_amps',exp_settings_all,'UniformOutput',0));
 % x = amps_all(:,1)*E_per_mA;
@@ -119,5 +138,3 @@ threshE = thresh_out.thresh;
 % [fitobj,gof,~] = fit(x,y,f);
 % inv_logit = @(y,a,b,c) c - log(a./y - 1)/b;% inverted logit, input coeffs and y, outputs x
 % thresh1 = inv_logit(0.5,fitobj.a,fitobj.b,fitobj.c);
-%% Plot strength duration curve
-pulse_durs_ms = [50e]
